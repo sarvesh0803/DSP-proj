a72e2b328ff6f613fcb339f7ee252f4a9d92c1ec
Kartik <kartik.iit96@gmail.com>
2017-11-30 06:04:18 +0530

    Update dsp_fft.py

--- dsp_fft.py
+++ dsp_fft.py
@@ -4,16 +4,7 @@ import matplotlib.pyplot as plt
 import scikits.audiolab as audio
 
 
-#reading audio file
-(inputSignal, samplingRate, bits) = audio.wavread('/home/kartik/Dataset/WAVs/new-classical/classical.00001.wav')
 
-
-new_signal=[]
-for p in xrange(0,len(inputSignal)-2,3):
-    new_signal.append((inputSignal[p]+inputSignal[p+1]+inputSignal[p+2])/3)
-
-new_signal=np.array(new_signal)
-siglen=len(new_signal)
 #function to calculate omega
 def omega(x,y):
     return cmath.exp((2.0*cmath.pi*1j*y)/x)
@@ -24,26 +15,7 @@ def padding_func(data):
    while k*512 < len(data):
       k += 1
    return np.concatenate((data, ([0] * (k*512 - len(data)))))
-frame_matrix=[]
-new=[]
-#--------------------------creating frames---------------------------------
 
-newest_signal=(padding_func(new_signal))
-
-signal_length=len(newest_signal)
-
-for i in xrange(0, (signal_length/512)):
-
-    for j in xrange(0,512):
-
-        new.append(newest_signal[(512*i)+j])
-
-    frame_matrix.append(new)
-    new=[]
-
-
-
-#fft implementation function
 def fft(signal):
    n = len(signal)
    if n == 1:
@@ -80,100 +52,151 @@ def center(signal):
     center/=n
     return center
 
+#-----------------------------------------------------------------------------
 
-#------------------------------Framing--------------------------------------
+#reading audio file
+direc=['/home/kartik/Dataset/WAVs/new-classical/classical.00001.wav','/home/kartik/Dataset/WAVs/new-rock/rock.00001.wav','/home/kartik/Dataset/WAVs/new-jazz/jazz.00001.wav','/home/kartik/Dataset/WAVs/new-metal/metal.00001.wav']
 
-
-
-frame_fft=[]
-frame_abs_output=[]
-roll_off=[]
-
-for i in xrange(0, len(frame_matrix)):
-    frame_fft.append(np.array(fft(frame_matrix[i])))
-
-for i in xrange(0, len(frame_matrix)):
-    frame_abs_output.append(np.array([abs(frame_fft[i][j]) for j in xrange(0, 512)]))
+for i in xrange(0,1):
 
 
+    (inputSignal, samplingRate, bits) = audio.wavread(direc[i])
 
 
-# ------------------------------------------Windowing---------------------------------------
+    new_signal=[]
+    for p in xrange(0,len(inputSignal)-2,3):
+        new_signal.append((inputSignal[p]+inputSignal[p+1]+inputSignal[p+2])/3)
 
-for i in xrange(0, len(frame_fft)):
-    frame_fft[i]=ham_window(frame_fft[i])
+    new_signal=np.array(new_signal)
+    siglen=len(new_signal)
+    frame_matrix=[]
+    new=[]
+    #--------------------------creating frames---------------------------------
 
+    newest_signal=(padding_func(new_signal))
 
+    signal_length=len(newest_signal)
 
+    for i in xrange(0, (signal_length/512)):
 
+        for j in xrange(0,512):
 
-# ---------------------------------------- Feature Extraction----------------------------------------
-#1. FFT
-fftoutput = np.array(fft(padding_func(new_signal)))
+            new.append(newest_signal[(512*i)+j])
 
-abs_output=np.array([abs(fftoutput[i]) for i in xrange(0, len(padding_func(new_signal)))])
-
-
-
-#2. power spectral density of the signal:
-power_spectral_density=np.array([abs_output[i]**2 for i in xrange(0, len(abs_output))])
+        frame_matrix.append(new)
+        new=[]
 
 
 
+    #fft implementation function
 
-#3. Spectral Centroid
+    #------------------------------Framing--------------------------------------
 
-spectral_centroid=[]
-centroid=0
-for i in xrange(0, len(frame_abs_output)):
-    for j in range(512):
-        centroid+=j*frame_abs_output[i][j]
 
-    spectral_centroid.append(centroid)
+
+    frame_fft=[]
+    frame_abs_output=[]
+    roll_off=[]
+    frame_PSD=[]
+
+    for i in xrange(0, len(frame_matrix)):
+        frame_fft.append(np.array(fft(frame_matrix[i])))
+
+    for i in xrange(0, len(frame_matrix)):
+        frame_abs_output.append(np.array([abs(frame_fft[i][j]) for j in xrange(0, 512)]))
+
+
+
+
+    # ------------------------------------------Windowing---------------------------------------
+
+    for i in xrange(0, len(frame_fft)):
+        frame_fft[i]=ham_window(frame_fft[i])
+
+
+
+
+
+
+    # ---------------------------------------- Feature Extraction--------------------
+    #1. FFT
+    fftoutput = np.array(fft(padding_func(new_signal)))
+
+    abs_output=np.array([abs(fftoutput[i]) for i in xrange(0, len(padding_func(new_signal)))])
+
+
+
+    #2. power spectral density of the signal:
+    power_spectral_density=np.array([abs_output[i]**2 for i in xrange(0, len(abs_output))])
+
+    for i in xrange(0, len(frame_fft)):
+        frame_PSD.append(np.array([abs(frame_fft[i][j])**2 for j in xrange(0, 512)]))
+
+
+
+    #3. Spectral Centroid
+
+    spectral_centroid=[]
     centroid=0
+    for i in xrange(0, len(frame_abs_output)):
+        for j in range(512):
+            centroid+=j*frame_abs_output[i][j]
+
+        spectral_centroid.append(centroid)
+        centroid=0
 
 
 
-#4. spectral roll_off
+    #4. spectral roll_off
 
 
-Threshold_Energy=[]
-
-mc=[]
-# find the spectral rolloff as the frequency position where the respective spectral energy is equal to c*totalEnergy
-
-for i in range(len(frame_matrix)):
-    Threshold_Energy.append(0.85*(np.sum(frame_abs_output[i] ** 2)))
-    CumSum = np.cumsum(frame_abs_output[i]**2)
-
-    [a, ]=np.nonzero(CumSum > Threshold_Energy[i])
-
-    if len(a) > 0:
-        mc.append(np.float64(a[0]) / (float(512)))
-    else:
-        mc.append(0.0)
-
-roll_off = np.mean(mc)
+    Threshold_Energy=[]
 
 
+    # find the spectral rolloff as the frequency position where the respective spectral energy is equal to c*totalEnergy
+    mc=[]
+    for i in range(len(frame_matrix)):
+        Threshold_Energy.append(0.85*(np.sum(frame_abs_output[i] ** 2)))
+        CumSum = np.cumsum(frame_abs_output[i]**2)
+
+        [a, ]=np.nonzero(CumSum > Threshold_Energy[i])
+
+        if len(a) > 0:
+            mc.append(np.float64(a[0]) / (float(512)))
+        else:
+            mc.append(0.0)
+
+    roll_off = np.mean(mc)
 
 
-#------------------Output Display-----------------------------
+    #------------------Output Display-----------------------------
 
 
-# print ("No of bins: " + len(bin))
+    # print ("No of bins: " + len(bin))
 
-print frame_matrix[2][3]
-print "\n"
-print ("Length of signal"+str(len(frame_matrix)))
-print ("Length of old signal"+str(len(frame_fft[5])))
 
-plt.plot(mc)
-plt.ylabel('Roll-off values')
-plt.xlabel('frames(n)')
-plt.show()
 
-plt.plot(frame_fft[5])
-plt.ylabel('FFT(x)')
-plt.xlabel('frequency(n)')
-plt.show()
+    print "\n"
+    print ("Length of signal"+str(len(frame_matrix)))
+    print ("Length of old signal"+str(len(frame_fft[5])))
+
+    plt.plot(frame_fft[5])
+    plt.ylabel('FFT(x)')
+    plt.xlabel('frequency(n)')
+    plt.show()
+
+
+    plt.plot(mc)
+    plt.ylabel('Roll-off values')
+    plt.xlabel('frames(n)')
+    plt.show()
+
+    plt.plot(spectral_centroid)
+    plt.ylabel('Spectral Centroid')
+    plt.xlabel('frequency(n)')
+    plt.show()
+
+    plt.plot(frame_PSD[5])
+    plt.ylabel('psd(x)')
+    plt.xlabel('frequency(n)')
+    plt.show()
